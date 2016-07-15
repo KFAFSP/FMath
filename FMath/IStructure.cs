1375a56529a55720cdf1cc2436e97a1aad8cbc44
Karl Friebel <karl.friebel@friebelnet.de>
2016-07-15 21:02:38 +0200

    IStructure interface created.

---
+++ FMath/IStructure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FMath
+{
+    /// <summary>
+    /// Interface for types that are structures consisting of multiple elements.
+    /// </summary>
+    public interface IStructure
+    {
+        /// <summary>
+        /// Gets the type of a structure element.
+        /// </summary>
+        /// <value>
+        /// The type of a structure element.
+        /// </value>
+        Type ElementType { get; }
+    }
+}
