a9a8f34d3255e01ce55785eb0f31ebf1e8d2a5f8
Karl Friebel <karl.friebel@friebelnet.de>
2016-07-15 22:54:52 +0200

    Implemented equality and hashing.

--- FMath/Linear/Static/Matrix.cs
+++ FMath/Linear/Static/Matrix.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+
+using FMath.Linear.Generic;
 
 namespace FMath.Linear.Static
 {
@@ -32,33 +35,175 @@ namespace FMath.Linear.Static
                    && AIndices.M < AMatrix.Size.M && AIndices.N < AMatrix.Size.N;
         }
 
-        #region Basic operations        
+        #region Equality
         /// <summary>
         /// Checks whether two matrices are equal by comparing their elements.
         /// </summary>
         /// <param name="ALeft">The left matrix.</param>
         /// <param name="ARight">The right matrix.</param>
         /// <returns><c>true</c> if they are equal, <c>false</c> otherwise.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
         [Pure]
         public static bool AreEqual(IMatrix ALeft, IMatrix ARight)
         {
-            // TODO : Implement
-            return false;
+            return Matrix.AreEqual(ALeft, ARight, Object.Equals);
         }
+        /// <summary>
+        /// Checks whether two matrices are equal by comparing their elements.
+        /// </summary>
+        /// <param name="ALeft">The left matrix.</param>
+        /// <param name="ARight">The right matrix.</param>
+        /// <param name="AEquator">The element equator.</param>
+        /// <returns><c>true</c> if they are equal, <c>false</c> otherwise.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
+        [Pure]
+        public static bool AreEqual(
+            IMatrix ALeft,
+            IMatrix ARight,
+            Func<object, object, bool> AEquator)
+        {
+            if (ALeft == null)
+                throw new ArgumentNullException("ALeft");
+            if (ARight == null)
+                throw new ArgumentNullException("ARight");
+            if (AEquator == null)
+                throw new ArgumentNullException("AEquator");
+
+            if (ALeft.Size != ARight.Size)
+                return false;
+
+            for (int M = 0; M < ALeft.Size.M; M++)
+                for (int N = 0; N < ARight.Size.N; N++)
+                {
+                    MatrixIndices miIndices = new MatrixIndices(M, N);
+                    if (!AEquator(ALeft.Get(miIndices), ARight.Get(miIndices)))
+                        return false;
+                }
+
+            return true;
+        }
+        /// <summary>
+        /// Checks whether two strongly typed matrices are equal by comparing their elements.
+        /// </summary>
+        /// <typeparam name="TLeft">The element type of the left matrix.</typeparam>
+        /// <typeparam name="TRight">The element type of the right matrix.</typeparam>
+        /// <param name="ALeft">The left matrix.</param>
+        /// <param name="ARight">The right matrix.</param>
+        /// <param name="AEquator">The element equator.</param>
+        /// <returns>
+        ///   <c>true</c> if they are equal, <c>false</c> otherwise.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
+        [Pure]
+        public static bool AreEqual<TLeft, TRight>(
+            IMatrix<TLeft> ALeft,
+            IMatrix<TRight> ARight,
+            Func<TLeft, TRight, bool> AEquator)
+        {
+            if (AEquator == null)
+                throw new ArgumentNullException("AEquator");
+
+            return Matrix.AreEqual((IMatrix)ALeft, ARight, (AL, AR) => AEquator((TLeft)AL, (TRight)AR));
+        }
+        /// <summary>
+        /// Checks whether two strongly typed matrices are equal by comparing their elements.
+        /// </summary>
+        /// <typeparam name="TData">The type of the stored data.</typeparam>
+        /// <param name="ALeft">The left matrix.</param>
+        /// <param name="ARight">The right matrix.</param>
+        /// <param name="AComparer">The element comparer, or null to use the default comparer.</param>
+        /// <returns>
+        ///   <c>true</c> if they are equal, <c>false</c> otherwise.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
+        [Pure]
+        public static bool AreEqual<TData>(
+            IMatrix<TData> ALeft,
+            IMatrix<TData> ARight,
+            IEqualityComparer<TData> AComparer = null)
+        {
+            if (AComparer == null)
+                AComparer = EqualityComparer<TData>.Default;
+
+            return Matrix.AreEqual(ALeft, ARight, AComparer.Equals);
+        }
+        #endregion
+
+        #region Hashing
         /// <summary>
         /// Hashes the specified matrix.
         /// </summary>
         /// <param name="AMatrix">A matrix.</param>
         /// <returns>An element-wise computed hash that also contains the size of the matrix.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
         [Pure]
         public static int Hash(IMatrix AMatrix)
         {
-            // TODO : Implement
-            return 0;
+            return Matrix.Hash(AMatrix, EqualityComparer<object>.Default.GetHashCode);
+        }
+        /// <summary>
+        /// Hashes the specified matrix.
+        /// </summary>
+        /// <param name="AMatrix">The matrix.</param>
+        /// <param name="AHasher">The element hashing delegate.</param>
+        /// <returns>An element-wise computed hash that also contains the size of the matrix.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
+        [Pure]
+        public static int Hash(
+            IMatrix AMatrix,
+            Func<object, int> AHasher)
+        {
+            if (AMatrix == null)
+                throw new ArgumentNullException("AMatrix");
+            if (AHasher == null)
+                throw new ArgumentNullException("AHasher");
+
+            int iHash = unchecked (AMatrix.Size.M*Vector.C_HashSaltPrime + AMatrix.Size.N);
+            for (int M = 0; M < AMatrix.Size.M; M++)
+                for (int N = 0; N < AMatrix.Size.N; N++)
+                    iHash = unchecked(iHash * Vector.C_HashSaltPrime + AHasher(AMatrix.Get(new MatrixIndices(M, N))));
+
+            return iHash;
+        }
+        /// <summary>
+        /// Hashes the specified strongly typed matrix.
+        /// </summary>
+        /// <typeparam name="TData">The type of the stored data.</typeparam>
+        /// <param name="AMatrix">The matrix.</param>
+        /// <param name="AHasher">The element hashing delegate.</param>
+        /// <returns>An element-wise computed hash that also contains the size of the matrix.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
+        [Pure]
+        public static int Hash<TData>(
+            IMatrix<TData> AMatrix,
+            Func<TData, int> AHasher)
+        {
+            if (AHasher == null)
+                throw new ArgumentNullException("AHasher");
+
+            return Matrix.Hash((IMatrix)AMatrix, AElement => AHasher((TData)AElement));
+        }
+        /// <summary>
+        /// Hashes the specified strongly typed matrix.
+        /// </summary>
+        /// <typeparam name="TData">The type of the stored data.</typeparam>
+        /// <param name="AMatrix">The matrix.</param>
+        /// <param name="AComparer">The element comparer, or null to use the default comparer.</param>
+        /// <returns>An element-wise computed hash that also contains the size of the matrix.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
+        [Pure]
+        public static int Hash<TData>(
+            IMatrix<TData> AMatrix,
+            IEqualityComparer<TData> AComparer = null)
+        {
+            if (AComparer == null)
+                AComparer = EqualityComparer<TData>.Default;
+
+            return Matrix.Hash(AMatrix, AComparer.GetHashCode);
         }
         #endregion
 
-        #region String formatting        
+        #region String formatting
         /// <summary>
         /// Formats the specified matrix.
         /// </summary>
--- FMath/Linear/Static/Vector.cs
+++ FMath/Linear/Static/Vector.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+
+using FMath.Linear.Generic;
 
 namespace FMath.Linear.Static
 {
@@ -31,33 +34,172 @@ namespace FMath.Linear.Static
             return AIndex >= 0 && AIndex < AVector.Size;
         }
 
-        #region Basic operations
+        #region Equality
         /// <summary>
         /// Checks whether two vectors are equal by comparing their elements.
         /// </summary>
         /// <param name="ALeft">The left vector.</param>
         /// <param name="ARight">The right vector.</param>
         /// <returns><c>true</c> if they are equal, <c>false</c> otherwise.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
         [Pure]
         public static bool AreEqual(IVector ALeft, IVector ARight)
         {
-            // TODO : Implement
-            return false;
+            return Vector.AreEqual(ALeft, ARight, Object.Equals);
         }
+        /// <summary>
+        /// Checks whether two vectors are equal by comparing their elements.
+        /// </summary>
+        /// <param name="ALeft">The left vector.</param>
+        /// <param name="ARight">The right vector.</param>
+        /// <param name="AEquator">The element equator delegate.</param>
+        /// <returns>
+        ///   <c>true</c> if they are equal, <c>false</c> otherwise.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
+        [Pure]
+        public static bool AreEqual(
+            IVector ALeft,
+            IVector ARight,
+            Func<object, object, bool> AEquator)
+        {
+            if (ALeft == null)
+                throw new ArgumentNullException("ALeft");
+            if (ARight == null)
+                throw new ArgumentNullException("ARight");
+            if (AEquator == null)
+                throw new ArgumentNullException("AEquator");
+
+            if (ALeft.Size != ARight.Size)
+                return false;
+
+            for (int I = 0; I < ALeft.Size; I++)
+                if (!AEquator(ALeft.Get(I), ARight.Get(I)))
+                    return false;
+
+            return true;
+        }
+        /// <summary>
+        /// Checks whether two strongly typed vectors are equal by comparing their elements.
+        /// </summary>
+        /// <typeparam name="TLeft">The element type of the left vector.</typeparam>
+        /// <typeparam name="TRight">The element type of the right vector.</typeparam>
+        /// <param name="ALeft">The left vector.</param>
+        /// <param name="ARight">The right vector.</param>
+        /// <param name="AEquator">The element equator delegate.</param>
+        /// <returns>
+        ///   <c>true</c> if they are equal, <c>false</c> otherwise.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
+        [Pure]
+        public static bool AreEqual<TLeft, TRight>(
+            IVector<TLeft> ALeft,
+            IVector<TRight> ARight,
+            Func<TLeft, TRight, bool> AEquator)
+        {
+            if (AEquator == null)
+                throw new ArgumentNullException("AEquator");
+
+            return Vector.AreEqual((IVector)ALeft, ARight, (AL, AR) => AEquator((TLeft)AL, (TRight)AR));
+        }
+        /// <summary>
+        /// Checks whether two strongly typed vectors are equal by comparing their elements.
+        /// </summary>
+        /// <typeparam name="TData">The type of the stored data.</typeparam>
+        /// <param name="ALeft">The left vector.</param>
+        /// <param name="ARight">The right vector.</param>
+        /// <param name="AComparer">The element comparer, or <c>null</c> to use the default comparer.</param>
+        /// <returns>
+        ///   <c>true</c> if they are equal, <c>false</c> otherwise.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
+        [Pure]
+        public static bool AreEqual<TData>(
+            IVector<TData> ALeft,
+            IVector<TData> ARight,
+            IEqualityComparer<TData> AComparer = null)
+        {
+            if (AComparer == null)
+                AComparer = EqualityComparer<TData>.Default;
+
+            return Vector.AreEqual(ALeft, ARight, AComparer.Equals);
+        }
+        #endregion
+
+        #region Hashing
         /// <summary>
         /// Hashes the specified vector.
         /// </summary>
         /// <param name="AVector">A vector.</param>
         /// <returns>An element-wise computed hash that also contains the size of the vector.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
         [Pure]
         public static int Hash(IVector AVector)
         {
-            // TODO : Implement
-            return 0;
+            return Vector.Hash(AVector, EqualityComparer<object>.Default.GetHashCode);
+        }
+        /// <summary>
+        /// Hashes the specified vector.
+        /// </summary>
+        /// <param name="AVector">The vector.</param>
+        /// <param name="AHasher">The element hashing delegate.</param>
+        /// <returns>An element-wise computed hash that also contains the size of the vector.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
+        [Pure]
+        public static int Hash(
+            IVector AVector,
+            Func<object, int> AHasher)
+        {
+            if (AVector == null)
+                throw new ArgumentNullException("AVector");
+            if (AHasher == null)
+                throw new ArgumentNullException("AHasher");
+
+            int iHash = AVector.Size;
+            for (int I = 0; I < AVector.Size; I++)
+                iHash = unchecked (iHash*Vector.C_HashSaltPrime + AHasher(AVector.Get(I)));
+
+            return iHash;
+        }
+        /// <summary>
+        /// Hashes the specified strongly typed vector.
+        /// </summary>
+        /// <typeparam name="TData">The type of the stored data.</typeparam>
+        /// <param name="AVector">The vector.</param>
+        /// <param name="AHasher">The element hashing delegate.</param>
+        /// <returns>An element-wise computed hash that also contains the size of the vector.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
+        [Pure]
+        public static int Hash<TData>(
+            IVector<TData> AVector,
+            Func<TData, int> AHasher)
+        {
+            if (AHasher == null)
+                throw new ArgumentNullException("AHasher");
+
+            return Vector.Hash((IVector)AVector, AElement => AHasher((TData)AElement));
+        }
+        /// <summary>
+        /// Hashes the specified strongly typed vector.
+        /// </summary>
+        /// <typeparam name="TData">The type of the stored data.</typeparam>
+        /// <param name="AVector">The vector.</param>
+        /// <param name="AComparer">The element comparer, or null to use the default comparer.</param>
+        /// <returns>An element-wise computed hash that also contains the size of the vector.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when any of the arguments is null.</exception>
+        [Pure]
+        public static int Hash<TData>(
+            IVector<TData> AVector,
+            IEqualityComparer<TData> AComparer = null)
+        {
+            if (AComparer == null)
+                AComparer = EqualityComparer<TData>.Default;
+
+            return Vector.Hash(AVector, AComparer.GetHashCode);
         }
         #endregion
 
-        #region String formatting        
+        #region String formatting
         /// <summary>
         /// Formats the specified vector.
         /// </summary>
