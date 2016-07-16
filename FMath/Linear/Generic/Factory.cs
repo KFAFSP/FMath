73f901770d5b3e60c966032113e17b070655e232
Karl Friebel <karl.friebel@friebelnet.de>
2016-07-16 02:07:08 +0200

    Added factory delegates.

---
+++ FMath/Linear/Generic/Factory.cs
@@ -0,0 +1,10 @@
+﻿namespace FMath.Linear.Generic
+{
+    public delegate IVector<TData> ImmutableVectorFactory<TData>(TData[] AElements);
+
+    public delegate IMutableVector<TData> MutableVectorFactory<TData>(int ASize);
+
+    public delegate IMatrix<TData> ImmutableMatrixFactory<TData>(TData[,] ACells);
+
+    public delegate IMutableMatrix<TData> MutableMatrixFactory<TData>(MatrixIndices ASize);
+}
