f1fddf36e48b40f4a79a51f88e2685b694f711ef
Karl Friebel <karl.friebel@friebelnet.de>
2016-07-16 16:12:56 +0200

    Temporarily excluded generic arithmetics.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using FMath.Arithmetics;
 using FMath.Linear.Generic;
 using FMath.Linear.Generic.Immutable;
 using FMath.Linear.Generic.Mutable;
@@ -32,16 +31,6 @@ namespace TestApp
             Console.WriteLine(Matrix.Format(imTest, "T"));
             Console.WriteLine(Matrix.Format(imTest, "e"));
             Console.WriteLine(Matrix.Format(imTest, "33"));
-
-            Console.WriteLine();
-            Console.WriteLine("Arithmetics");
-            Console.WriteLine(ArithmeticProvider<float>.Instance);
-
-            ArrayVector<int> av1 = ArrayVector.Pack(0, 1, -2);
-            ArrayVector<int> av2 = ArrayVector.Pack(1, -1, 1);
-            DenseVector<int> dv1 = new DenseVector<int>(3);
-            //NumericVector.Subtract(av1, av2, dv1);
-            Console.WriteLine(dv1.ToString());
 
             Console.ReadLine();
         }
