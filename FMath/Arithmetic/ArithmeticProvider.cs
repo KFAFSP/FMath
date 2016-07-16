b4ed9dce1cb3eb225ce178d661148331c555919f
Karl Friebel <karl.friebel@friebelnet.de>
2016-07-16 19:45:15 +0200

    Reimplemented inner generic arithmetics.

--- FMath/Arithmetic/ArithmeticProvider.cs
+++ FMath/Arithmetic/ArithmeticProvider.cs
@@ -1,7 +1,58 @@
-﻿namespace FMath.Arithmetic
+﻿using System;
+using System.Reflection;
+
+namespace FMath.Arithmetic
 {
     public abstract class ArithmeticProvider<TNumeral>
     {
+        public static readonly ArithmeticProvider<TNumeral> Instance;
+
+        static ArithmeticProvider()
+        {
+            Type tNumeral = typeof(TNumeral);
+            Assembly aAssembly = Assembly.GetCallingAssembly();
+
+            foreach (Type tType in aAssembly.GetTypes())
+            {
+                if (!tType.IsClass || tType.IsAbstract)
+                    continue;
+                if (tType.BaseType == null)
+                    continue;
+                if (!tType.BaseType.IsConstructedGenericType || tType.BaseType.GenericTypeArguments.Length != 1)
+                    continue;
+                if (tType.BaseType.GenericTypeArguments[0] != tNumeral)
+                    continue;
+                if (!typeof(ArithmeticProvider<TNumeral>).IsAssignableFrom(tType))
+                    continue;
+
+                ConstructorInfo ciCreate = tType.GetConstructor(new Type[0]);
+
+                if (ciCreate == null)
+                    continue;
+
+                ArithmeticProvider<TNumeral>.Instance = (ArithmeticProvider<TNumeral>)ciCreate.Invoke(new object[0]);
+                return;
+            }
+        }
+
+        public static bool Exists
+        {
+            get { return ArithmeticProvider<TNumeral>.Instance != null; }
+        }
+
+        public NaturalArithmeticProvider<TNumeral> AsNatural()
+        {
+            return this as NaturalArithmeticProvider<TNumeral>;
+        }
+        public IntegerArithmeticProvider<TNumeral> AsInteger()
+        {
+            return this as IntegerArithmeticProvider<TNumeral>;
+        }
+        public FractionalArithmeticProvider<TNumeral> AsFractional()
+        {
+            return this as FractionalArithmeticProvider<TNumeral>;
+        }
+
         public abstract NumeralType Type { get; }
     }
 }
---
+++ FMath/Arithmetic/InnerGenericArithmetics.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace FMath.Arithmetic
+{
+    public static class InnerGenericArithmetics
+    {
+        public static NumeralType Type<TNumeral>()
+        {
+            return ArithmeticProvider<TNumeral>.Instance.Type;
+        }
+
+        public static TNumeral Add<TNumeral>(TNumeral ALeft, TNumeral ARight)
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsNatural().Add(ALeft, ARight);
+        }
+        public static TNumeral Multiply<TNumeral>(TNumeral ALeft, TNumeral ARight)
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsNatural().Multiply(ALeft, ARight);
+        }
+        public static TNumeral Div<TNumeral>(TNumeral ALeft, TNumeral ARight)
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsNatural().Div(ALeft, ARight);
+        }
+        public static TNumeral Mod<TNumeral>(TNumeral ALeft, TNumeral ARight)
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsNatural().Mod(ALeft, ARight);
+        }
+
+        public static bool IsModular<TNumeral>()
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsNatural().IsModular;
+        }
+        public static TNumeral Zero<TNumeral>()
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsNatural().Zero;  
+        }
+        public static TNumeral One<TNumeral>()
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsNatural().One; 
+        }
+
+        public static TNumeral Sign<TNumeral>(TNumeral ALeft)
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsInteger().Sign(ALeft);
+        }
+        public static TNumeral Absolute<TNumeral>(TNumeral ALeft)
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsInteger().Absolute(ALeft);
+        }
+        public static TNumeral Negate<TNumeral>(TNumeral ALeft)
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsInteger().Negate(ALeft);
+        }
+
+        public static TNumeral Invert<TNumeral>(TNumeral ALeft)
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsFractional().Invert(ALeft);
+        }
+        public static TNumeral Ceil<TNumeral>(TNumeral ALeft)
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsFractional().Ceil(ALeft);
+        }
+        public static TNumeral Trunc<TNumeral>(TNumeral ALeft)
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsFractional().Trunc(ALeft);
+        }
+        public static TNumeral Round<TNumeral>(TNumeral ALeft, MidpointRounding AMidpointStrategy)
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsFractional().Round(ALeft, AMidpointStrategy);
+        }
+        public static TNumeral Frac<TNumeral>(TNumeral ALeft)
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsFractional().Frac(ALeft);
+        }
+        public static TNumeral Divide<TNumeral>(TNumeral ALeft, TNumeral ARight)
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsFractional().Divide(ALeft, ARight);
+        }
+
+        public static TNumeral Infinity<TNumeral>()
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsFractional().Infinity; 
+        }
+        public static TNumeral NaN<TNumeral>()
+        {
+            return ArithmeticProvider<TNumeral>.Instance.AsFractional().NaN; 
+        }
+    }
+}
